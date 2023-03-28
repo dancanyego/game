6ba2fb20939a80981933676cc05e6bb4fc3d5fb7
Dancan Yego <dancankipchumba76@gmail.com>
2023-03-29 01:00:15 +0300

    account bal

--- OOP/project1.py
+++ OOP/project1.py
@@ -10,10 +10,25 @@ class Account():
     def __init__(self,name,balance):
         self.name = name
         self.balance = balance
-        
-        acc1 = Account("name", 200)
-        
-        print(acc1)
 
-    def __init__(self,deposit,withdrawal):
-        pass
+        
+    def __str__(self):
+        print(f"Account owner :> {self.name}")
+        print(f"Account balance :> {self.balance}")
+        
+  
+
+    def deposit(self,dep_amt):
+        self.balance += dep_amt
+        print("Deposit Success!")
+        
+    def withdrawal(self,rem_amt):
+        if self.balance >= rem_amt:
+            self.balance -= rem_amt
+            
+        else:
+            print("Insufficient Funds!!")
+            
+            
+acct1 = Account('Jose',100)
+        
