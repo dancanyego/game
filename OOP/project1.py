914b380de4b4855dbad5450b2da6f6b63a7544de
Dancan Yego <dancankipchumba76@gmail.com>
2023-03-29 00:34:21 +0300

    accounts

---
+++ OOP/project1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 00:20:36 2023
+
+@author: danca
+"""
+
+class Account():
+    
+    def __init__(self,name,balance):
+        self.name = name
+        self.balance = balance
+        
+        acc1 = Account("name", 200)
+        
+        print(acc1)
+
+    def __init__(self,deposit,withdrawal):
+        pass
