2e17e3d705d9ff181e7166394125b0e33d320643
Dancan Yego <dancankipchumba76@gmail.com>
2023-03-29 23:34:15 +0300

    error handling

--- errors_handling/error.py
+++ errors_handling/error.py
@@ -2,7 +2,44 @@ def add(n1,n2):
     print(n1+ n2)
 add(30, 50)    
 
-number_1 = int(input("Enter Your first Number"))
-number_2 = int(input("Enter Your first Number"))
+number_1 = int(input("Enter Your first Number  "))
+number_2 = int(input("Enter Your 2nd Number  "))
 
 add(number_1, number_2)
+
+
+try:
+    pass
+except:
+    pass
+
+
+try:
+    f = open('testfile','w')
+    f.write("This is a test file")
+except TypeError:
+    print("There Waas a type error")
+    
+except OSError:
+    print("OS Error was found")
+finally:
+    print("I Always Run nigga")
+    
+    
+def ask_for_int():
+    while True:
+        try:
+            result = int(input("PLease Provide your number "))
+            
+        except:
+            print("Whoopsie not a number!!")
+            continue
+        else:
+            print("Finnaly nigga")
+            break
+            
+        finally:
+            print("End of Block ")
+        
+ask_for_int()
+    
