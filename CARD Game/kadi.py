825d65c6055eeb76be098acd24d7622c01677ee8
Dancan Yego <dancankipchumba76@gmail.com>
2023-03-31 23:13:32 +0300

    shuffling cards

--- CARD Game/kadi.py
+++ CARD Game/kadi.py
@@ -12,11 +12,42 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
             'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
+    
     def __init__(self,suit,rank):
         self.suit = suit
-        self.rank - rank
+        self.rank = rank
         self.value = values[rank]
         
     def __str__(self):
-        return self.rank + " of " + self.suit
+        return self.rank + ' of ' + self.suit
+    
+
+#Deck class 
+
+class Deck():
+    
+    def __init__(self):
+        
+        self.all_cards = []
+        
+        for suit in suits:
+            for rank in ranks:
+                #Creating the card object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+                
+    def shuffle(self):
+        
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop()
+                
+new_deck = Deck()
+bottom_card =new_deck.all_cards[-1]
+
+#for card_object in new_deck.all_cards: 
+#  print(card_object)
+
+
     
