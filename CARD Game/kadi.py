bb8963ea7b2ac8cde90e1250f9451f871925f923
Dancan Yego <dancankipchumba76@gmail.com>
2023-03-31 22:33:41 +0300

    Deck Class

--- CARD Game/kadi.py
+++ CARD Game/kadi.py
@@ -4,13 +4,19 @@ Created on Fri Mar 31 00:06:55 2023
 
 @author: danca
 """
+import random
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
     def __init__(self,suit,rank):
         self.suit = suit
         self.rank - rank
+        self.value = values[rank]
         
     def __str__(self):
         return self.rank + " of " + self.suit
-    
     
