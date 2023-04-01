9084b8346195e7239bbbde76bac583c95a210e01
Dancan Yego <dancankipchumba76@gmail.com>
2023-04-01 16:49:32 +0300

    game on

--- kadi.py
+++ kadi.py
@@ -12,11 +12,88 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
             'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 class Card:
+    
     def __init__(self,suit,rank):
         self.suit = suit
-        self.rank - rank
+        self.rank = rank
         self.value = values[rank]
         
     def __str__(self):
-        return self.rank + " of " + self.suit
+        return self.rank + ' of ' + self.suit
     
+
+#Deck class 
+
+class Deck():
+    
+    def __init__(self):
+        
+        self.all_cards = []
+        
+        for suit in suits:
+            for rank in ranks:
+                #Creating the card object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+                
+    def shuffle(self):
+        
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        return self.all_cards.pop()
+                
+new_deck = Deck()
+new_deck.shuffle()
+mycard = new_deck.deal_one()
+print(mycard)
+print(len(new_deck.all_cards))
+bottom_card =new_deck.all_cards[-1]
+
+#for card_object in new_deck.all_cards: 
+#  print(card_object)
+
+#Player Class
+
+class Player:
+    
+    def __init__(self,name):
+        self.name = name
+        # A new player has no cards
+        self.all_cards = [] 
+        
+    def remove_one(self):
+        # Note we remove one card from the list of all_cards
+        # We state 0 to remove from the "top" of the deck
+        # We'll imagine index -1 as the bottom of the deck
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+new_player = Player('Jomba')
+print(new_player)
+new_player.add_cards(mycard)
+print(new_player)
+        
+# Game Logic
+
+player_one = Player(input("Enter Your Prefered name 4 player 1 >> "))
+player_two = Player(input("Enter Your Prefered name 4 player 2 >> "))
+
+new_deck = Deck()
+new_deck.shuffle()
+
+# Splitting cards into two
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+    
+z = len(player_one.all_cards)
+print(z)
